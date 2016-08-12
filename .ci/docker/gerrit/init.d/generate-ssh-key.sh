fa7deb2a5e9c8da546f84fb12c67495dd22738ee
Oliver Palmer <oliverpalmer@opalmer.com>
2016-08-12 09:40:30 -0400

    trying out postgres
    Trying a switch to postgres.  Startup time is quite a bit faster
    and it's easiear to 'preload' test data.

--- .ci/docker/gerrit/init.d/generate-ssh-key.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-mkdir -p /var/gerrit/.ssh/
-ssh-keygen -t rsa -f /var/gerrit/.ssh/id_rsa -N ""
-cp /var/gerrit/.ssh/id_rsa.pub /var/gerrit/.ssh/authorized_keys
-chown -R gerrit2:gerrit2 /var/gerrit/.ssh/
-chmod 700 /var/gerrit/.ssh/
-chmod 600 /var/gerrit/.ssh/id_rsa
--- .ci/docker/gerrit/init.d/move-config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -xe
-
-mv /var/gerrit/gerrit.config /var/gerrit/review_site/etc/gerrit.config
