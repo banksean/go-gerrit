5093a3457af9b3a10dde7d7d0f9f62a4b078069c
Oliver Palmer <oliverpalmer@opalmer.com>
2016-10-16 10:52:44 -0400

    fix url

--- .ci/travis/start-gerrit.sh
+++ .ci/travis/start-gerrit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -f helper-functions.sh ] || curl -L -o helper-functions.sh https://raw.githubusercontent.com/opalmer/docker-gerrittest/master/helper-functions.sh --fail
+[ -f helper-functions.sh ] || curl -L -o helper-functions.sh https://raw.githubusercontent.com/opalmer/gerrittest/master/docker/helper-functions.sh --fail
 . helper-functions.sh
 
 RunContainer
